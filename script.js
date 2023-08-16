ecbf72f02419757bb216bacbaf75695870c43612
gilberthappi <gdushimimana6@gmail.com>
2023-08-17 00:19:05 +0200

    Adding Files and Folders for Linters and ESLinter

--- script.js
+++ script.js
@@ -1,65 +1,33 @@
-const booksData = [];
+class Book {
+  constructor(title, author, id = Math.floor(Math.random() * 10000)) {
+    this.title = title;
+    this.author = author;
+    this.id = id;
+  }
 
-function removebook(bookId) {
-  let storedData = JSON.parse(localStorage.getItem('storedData'));
-  storedData = storedData.filter((bookobj) => {
-    if (bookobj.id.toString() === bookId) {
-      return false;
-    }
-    return true;
-  });
-  localStorage.setItem('storedData', JSON.stringify(storedData));
-}
-
-function createBook(book) {
-  const booksCont = document.getElementById('booksCont');
-  const bookDiv = document.createElement('div');
-  bookDiv.className = 'book';
-  bookDiv.id = `book-${book.id}`;
-  const bookElement = `
-        <h4 class="bookTitle">${book.title}</h4>
-        <p class="bookAuthor">${book.author}</p>
+  add() {
+    const booksCont = document.getElementById('booksCont');
+    const bookDiv = document.createElement('div');
+    bookDiv.className = 'book';
+    bookDiv.id = `book-${this.id}`;
+    const bookElement = `
+        <h4 class="bookTitle">${this.title}</h4>
+        <p class="bookAuthor">${this.author}</p>
         <button class="removeBtn">Remove</button>
         <hr>
       `;
-  bookDiv.innerHTML += bookElement;
-  bookDiv.querySelector('.removeBtn').addEventListener('click', (event) => {
-    const book = document.getElementById(event.target.parentElement.id);
-    book.parentElement.removeChild(book);
-    const bookId = book.id.substring(5, book.id.length);
-    removebook(bookId);
-  });
-  booksCont.append(bookDiv);
-}
-
-function storeInputData() {
-  const inputData = {
-    title: document.getElementById('title').value,
-    author: document.getElementById('author').value,
-    id: Math.floor(Math.random() * 10000),
-  };
-  const storedData = JSON.parse(localStorage.getItem('storedData'));
-  storedData.push(inputData);
-  localStorage.setItem('storedData', JSON.stringify(storedData));
-  createBook(inputData);
-}
-
-const initLoad = (booksData) => {
-  const storedData = JSON.parse(localStorage.getItem('storedData'));
-  if (storedData) {
-    storedData.forEach((book) => {
-      createBook(book);
+    bookDiv.innerHTML += bookElement;
+    const inputData = {
+      title: this.title,
+      author: this.author,
+      id: this.id,
+    };
+    const storedData = JSON.parse(localStorage.getItem('storedData'));
+    storedData.push(inputData);
+    localStorage.setItem('storedData', JSON.stringify(storedData));
+    bookDiv.querySelector('.removeBtn').addEventListener('click', () => {
+      this.remove();
     });
-  } else {
-    localStorage.setItem('storedData', JSON.stringify(booksData));
-    booksData.forEach((book) => {
-      createBook(book);
-    });
+    booksCont.append(bookDiv);
   }
-  const addBtn = document.getElementById('addBtn');
-  addBtn.addEventListener('click', () => {
-    storeInputData();
-  });
-};
-
-initLoad(booksData);
+//Add code here
