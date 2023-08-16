2379bb202675d32f0f09942884f1e81d53245dfc
smas0900 <47197167+smas0900@users.noreply.github.com>
2023-08-17 01:52:25 +0500

    Add create book fucntion for population the book section

--- script.js
+++ script.js
@@ -17,7 +17,32 @@ const booksData = [
   },
 ];
 
-
+function createBook(book) {
+  const booksCont = document.getElementById('booksCont');
+  const bookDiv = document.createElement('div');
+  bookDiv.className = 'book';
+  bookDiv.id = `book-${book.id}`;
+  const bookElement = `
+        <h4 class="bookTitle">${book.title}</h4>
+        <p class="bookAuthor">${book.author}</p>
+        <button class="removeBtn">Remove</button>
+        <hr>
+      `;
+  bookDiv.innerHTML += bookElement;
+  bookDiv.querySelector('.removeBtn').addEventListener('click', (event) => {
+    const book = document.getElementById(event.target.parentElement.id);
+    book.parentElement.removeChild(book);
+    let storedData = JSON.parse(localStorage.getItem('storedData'));
+    storedData = storedData.filter((bookobj) => {
+      if (bookobj.id.toString() === book.id.substring(5, book.id.length)) {
+        return false;
+      }
+      return true;
+    });
+    localStorage.setItem('storedData', JSON.stringify(storedData));
+  });
+  booksCont.append(bookDiv);
+}
 
 function storeInputData() {
   const inputData = {
